d23816f0ef11f460221860d5bab85fc48702dbde
baitul1900 <baitulamin1900@gmail.com>
2026-04-23 23:45:05 +0600

    feat: update VideoSection heading copy and replace background asset

--- app/home-components/VideoSection.jsx
+++ app/home-components/VideoSection.jsx
@@ -40,8 +40,10 @@ const VideoSection = () => {
           transition={{ duration: 1.2, ease: [0.16, 1, 0.3, 1] }}
           className="text-[#FFE6D0] text-[40px] md:text-[80px] lg:text-[116px] font-medium font-[var(--font-founders)] uppercase leading-[1.1] md:leading-[90px] lg:leading-[100px] tracking-[-2px] md:tracking-[-6px] lg:tracking-[-11.6px]"
         >
-          Discover our
-          <br className="hidden md:block" /> reality
+          Our work
+          <br className="hidden md:block" /> is not about ornament.
+          <br className="hidden md:block" /> It is about alignment.
+
         </motion.h2>
 
         <motion.div
